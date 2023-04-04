8874816c1803d779d99f8d5b682fc7696f76bbb6
SgtDrewster <129903481+SgtDrewster@users.noreply.github.com>
2023-04-04 17:18:40 -0400

    Server.lua

--- SteamToJoin/server.lua
+++ SteamToJoin/server.lua
@@ -2,7 +2,7 @@ AddEventHandler('playerConnecting',function(Name,setKickReason,deferrals)
 	for _, v in ipairs(GetPlayerIdentifiers(source)) do
 		if string.find(v,"license") then
 			setKickReason("Steam has to be open to join this server!") -- Message that gets displayed when Denied Connection
-			CancelEvent() -- Rejects User When Attempting to Join without Steam (Do Not Touch)
+			CancelEvent() -- Rejects User When Attempting to Join without Steam (Do Not Touch) 
 			break
 		end
 	end
